e480934f8fe202a13341bb4f5781cd8099395a11
KEERTHINAIK R <keerthinaik.harika@gmail.com>
2020-07-10 20:36:04 +0530

    inreased performance by adding shouldUpdateCheck in modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Aux from '../../../hoc/Auxilary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component{
 
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
+    // This could be a functional component for perfrmance testing we made it as class component
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("[OrderSummary] ComponentDidUpdate");
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p>Continue to Checkout?</p>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelHandler}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinueHandler}>CONTINUE</Button>
+            </Aux>
         );
-    });
+    }
+}
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to Checkout?</p>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <Button btnType="Danger" clicked={props.purchaseCancelHandler}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinueHandler}>CONTINUE</Button>
-        </Aux>
-    )
-};
-
-export default orderSummary;
+export default OrderSummary;
